f2b24f55651422db282da41077e738fd54bb5e3a
thilux <thilux.systems@gmail.com>
2018-03-26 17:42:19 -0300

    Adding github URL to project setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     version=find_version("sshmetroserver", "__init__.py"),
     description="An SSH tunnel server.",
     long_description=long_description,
+    url='https://github.com/thilux/ssh_metro_server',
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -73,7 +74,7 @@ setup(
         ],
     },
     tests_require=requires,
-    install_require=requires,
+    install_requires=requires,
     zip_safe=False,
     python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     extras_require={
